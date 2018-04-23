d2b95df2170da3f1d3ba6c8ebf3e9448a1215189
wing-0 <38071649+wing-0@users.noreply.github.com>
2018-04-23 16:53:28 +0200

    Add some code in spanning

--- Spanning usa/src/spanning.java
+++ Spanning usa/src/spanning.java
@@ -1,4 +1,31 @@
 
-public class spanning {
+public class spanning
+{
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	private List<Highway> prim(graph, rootNode)
+	
+}
 
+class Highway
+{
+	private String city1;
+	private String city2;
+	private int dist;
+	
+	public Highway(String city1, String city2, int dist)
+	{
+		this.city1 = city1;
+		this.city2 = city2;
+		this.dist = dist;
+	}
+}
+
+class ReadGraph
+{
+	
 }
