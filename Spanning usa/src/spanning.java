0d82acdf150220b754f988d0cc6394f01da9ed7f
wing-0 <38071649+wing-0@users.noreply.github.com>
2018-04-24 17:30:19 +0200

    Add some skeleton for Prim's alg

--- Spanning usa/src/spanning.java
+++ Spanning usa/src/spanning.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class spanning
@@ -11,6 +12,31 @@ public class spanning
 	private LinkedList<Integer[]> prim(int[][] graph, int rootNode)
 	{
 		LinkedList<Integer[]> tree = new LinkedList<Integer[]>();
+		ArrayList<Integer> nodesDone = new ArrayList<Integer>(graph.length);
+		nodesDone.add(rootNode);
+		ArrayList<Integer> nodesLeft = new ArrayList<Integer>(graph.length);
+		for(int i = 0; i < graph.length; i++)
+		{
+			if(i != rootNode)
+			{
+				nodesLeft.add(i);
+			}
+		}
+		
+		while(!nodesLeft.isEmpty())
+		{
+			// Search for a node in nodesLeft which minimizes distance
+			// to nodes in nodesDone
+			int minNode = -1;
+			int minDist = Integer.MAX_VALUE;
+			for(int outNode : nodesLeft)
+			{
+				for(int inNode : nodesDone)
+				{
+					
+				}
+			}
+		}
 	}
 	
 }
