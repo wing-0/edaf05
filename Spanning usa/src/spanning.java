76dd8be12ffb356f54791568e5585a7933ccc0c7
tfy13jwa <38432624+tfy13jwa@users.noreply.github.com>
2018-04-24 17:35:47 +0200

    add buffered reader in spanning main

--- Spanning usa/src/spanning.java
+++ Spanning usa/src/spanning.java
@@ -1,11 +1,23 @@
 import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class spanning
 {
 	
 	public static void main(String[] args)
 	{
-		
+		BufferedReader in = new BufferedReader(new FileReader(args[0]));
+		String currentLine;
+		Map<String,Integer> cities = new HashMap<String,Integer>();
+		int i = 0;
+
+		while(currentLine = in.readLine() != null 
+			&& currentLine.charAt(currentLine.length()-1) != "]")
+		{
+			cities.put(currentLine, i)
+		}
 	}
 	
 	private LinkedList<Integer[]> prim(int[][] graph, int rootNode)
